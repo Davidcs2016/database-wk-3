fd42f44725a2b44da9f8a74ee834531cdc152b4a
Dawit Uta <davecs2006@gmail.com>
2025-09-22 02:40:15 +0300

    Add files via upload

--- answers.sql
+++ answers.sql
@@ -1,1 +1,7 @@
-
+create table student (id integer PRIMARY KEY,
+    fullName varchar(100),
+    age int);
+ insert into student (id, fullName, age) values (1, "David Uta",33);
+ insert into student (id, fullName, age) values (2, "Mark Zucker",40);
+insert into student (id, fullName, age) values (3, "Elon Mask",60);
+update student set age=20 where id=2;
